7129a10b8eb0ad26dd2f10f34a718737781d277e
hwahyang1 <40535405+hwahyang1@users.noreply.github.com>
2019-03-25 23:34:08 +0900

    Update setting.py

--- setting.py
+++ setting.py
@@ -26,3 +26,24 @@ class Settings:
         # Naver API
         self.naver_api_id = "id"
         self.naver_api_secret = "secret"
+
+# ------ 이 아래는 건들지 마세요 ----- #
+
+import ctypes
+
+class MEMORYSTATUSEX(ctypes.Structure):
+    _fields_ = [
+        ("dwLength", ctypes.c_ulong),
+        ("dwMemoryLoad", ctypes.c_ulong),
+        ("ullTotalPhys", ctypes.c_ulonglong),
+        ("ullAvailPhys", ctypes.c_ulonglong),
+        ("ullTotalPageFile", ctypes.c_ulonglong),
+        ("ullAvailPageFile", ctypes.c_ulonglong),
+        ("ullTotalVirtual", ctypes.c_ulonglong),
+        ("ullAvailVirtual", ctypes.c_ulonglong),
+        ("sullAvailExtendedVirtual", ctypes.c_ulonglong),
+    ]
+
+    def __init__(self):
+        self.dwLength = ctypes.sizeof(self)
+        super(MEMORYSTATUSEX, self).__init__()
